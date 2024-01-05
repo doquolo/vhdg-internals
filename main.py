aa0e9b2226ffc911c8af3af8efddf214fe43483b
doquolo <doquolo123@outlook.com>
2024-01-06 00:51:32 +0700

    gui done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, request
+import json
 
 app = Flask(__name__)
 
@@ -8,6 +9,25 @@ def homepage():
 
 @app.route("/assets")
 def serveAssets():
+    args = request.args
+    img_name = args.get("image", default="", type=str)
+    return send_file(f"assets//{img_name}")
+
+@app.route("/foodlist")
+def serveFoodlist():
+    with open("data/foodlist.json", "r", encoding="utf-8") as file:
+        food_obj = json.loads(file.read())
+        food_list = {}
+        for id in food_obj:
+            food_list[id] = food_obj[id]["name"]
+        return food_list
     
+@app.route("/price")
+def price():
+    args = request.args
+    id = args.get("id", default="", type=str)
+    with open("data/foodlist.json", "r", encoding="utf-8") as file:
+        food_obj = json.loads(file.read())
+        return food_obj[id]["price"]
 
 app.run("0.0.0.0", 80, True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,22 @@
 var foodlist = {}
 
+document.addEventListener("DOMContentLoaded", () => {
+    initList();
+});
+
+async function initList() {
+    const menu_list = document.querySelector("#menu-list");
+    menu_list.innerHTML = "";
+
+    const request = await fetch("/foodlist");
+    const list = await request.json();
+    for (let i in list) {
+        menu_list.innerHTML += `
+        <div class="item" onclick="addToList(this, ${i})" style="background-image: url('/assets?image=${i}.png')">${list[i]}</div>
+        `;
+    }
+}
+
 const addToList = (food, id) => {
     if (foodlist[id] == undefined) {
         foodlist[id] = {'name': food.textContent, 'price': 0, 'amount': 1}
@@ -10,11 +27,43 @@ const addToList = (food, id) => {
     updateList();
 }
 
-const updateList = () => {
-    const checkout_list = document.querySelector("#checkout-list");
-    checkout_list.innerHTML = "";
-    for (let i in foodlist) {
-        checkout_list.innerHTML += 
-        `<div class="item"> x${foodlist[i]["amount"]} ${foodlist[i]["name"]}</div>`;
+const substractFood = (id) => {
+    if (foodlist[id]['amount'] <= 1) {
+        delete foodlist[id];
+    } else {
+        foodlist[id]['amount'] -= 1;
     }
+    //update
+    updateList();
+}
+
+async function updateList() {
+    const checkout_list = document.querySelector("#checkout-list");
+    checkout_list.innerHTML = `<div class="item header">
+    <div class="text">Món ăn</div>
+    <div class="price">Đơn giá</div>
+    <div class="price">Tổng</div>
+</div>`;
+    var total = 0;
+    for (let i in foodlist) {
+        const price = await (await fetch(`/price?id=${i}`)).json();
+        const subtotal = Number(foodlist[i]["amount"]) * price;
+        total += subtotal;
+
+        checkout_list.innerHTML += 
+        `
+        <div class="item" onclick="substractFood(${i})">
+            <div class="text"><p>x${foodlist[i]["amount"]}</p><p>${foodlist[i]["name"]}</p></div>
+            <div class="price">${price}.000đ</div>
+            <div class="price">${subtotal}.000đ</div>
+        </div>
+        `
+    }
+    checkout_list.innerHTML += 
+        `
+        <div class="item header">
+        <div class="text">Tổng đơn</div>
+        <div class="price">${total}.000đ</div>
+    </div>
+        `;
 }
